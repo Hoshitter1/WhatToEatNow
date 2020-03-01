76c4c6c64210dc7ae3fc74c1b27510275cb56aa4
Hoshitter1 <hoshi.to.318@gmail.com>
2020-03-01 20:02:56 +0900

    Update #24

--- django/line_bot_app/views.py
+++ django/line_bot_app/views.py
@@ -1,5 +1,6 @@
 import os
 from dataclasses import dataclass
+from typing import Dict, Tuple
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -14,6 +15,7 @@ from linebot.models import (
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseForbidden, HttpResponse
 
+from . import search_recipe
 from user_detail.models import UserDetail
 
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
@@ -22,74 +24,69 @@ line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 
-def _tmp_food_search_func(like_ingredients, num_of_trial=5):
-    for i in range(num_of_trial):
-        ingredients = 'kinoko'
-        if ingredients in like_ingredients:
-            break
-    return 'url'
+@csrf_exempt
+def callback(self):
+    """
+    Webhook request always comes here first
+    so handler will be enabled.
+    """
+    # This needs to be updated
+    signature = self.META['HTTP_X_LINE_SIGNATURE']
+    body = self.body.decode('utf-8')
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        HttpResponseForbidden()
+    return HttpResponse('OK', status=200)
 
 
 class LineBotBase:
-
     @csrf_exempt
     def callback(self):
         """
-        Webhook request always comes here first
-        so handler will be enabled.
+        create callback here
         """
-        # This needs to be updated
-        signature = self.META['HTTP_X_LINE_SIGNATURE']
-        body = self.body.decode('utf-8')
-        try:
-            handler.handle(body, signature)
-        except InvalidSignatureError:
-            HttpResponseForbidden()
-        return HttpResponse('OK', status=200)
-
-    @handler.add(AccountLinkEvent)
-    def auth_notification(self, event):
-        nonce = event.link.nonce
-        # TODO: Search account uid from the nonce, and link msg_user_id to profile_uid
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="認証できたで！！！！！"
-                                 f"your nonce is {nonce}"
-                                 f"your id is {event.source.user_id}")
-        )
+        pass
+    # @handler.add(AccountLinkEvent)
+    # def auth_notification(self, event):
+    #     nonce = event.link.nonce
+    #     # TODO: Search account uid from the nonce, and link msg_user_id to profile_uid
+    #     line_bot_api.reply_message(
+    #         event.reply_token,
+    #         TextSendMessage(text="認証できたで！！！！！"
+    #                              f"your nonce is {nonce}"
+    #                              f"your id is {event.source.user_id}")
+    #     )
 
 
 @dataclass
-class LineBot(LineBotBase):
+class MessageLineBot(LineBotBase):
     line_message_uid = None
     message_from_user = None
 
     @handler.add(MessageEvent, message=TextMessage)
     def handle_message(self, event) -> None:
         self.message_from_user = event.message.text
-        test_terms = ("テスト")
-        linking_terms = ("連携", 'れんけい')
-        all_terms = (
-            *test_terms,
-            *linking_terms,
-        )
 
-        # If message does not include any terms specified, do nothing.
-        if self.message_from_user not in all_terms:
+        # Key is name of module, values is key terms for triggering
+        foo: Dict[str, Tuple[str]] = {
+            'test': ('テスト',),
+            'linking_terms': ("連携", 'れんけい',)
+            #     Add more actions here
+        }
+        if self.message_from_user not in foo.values():
             return
 
+        for key in foo.keys():
+            if self.message_from_user in foo.get(key):
+                msg_handler = getattr(self, key)
+                break
         self.line_message_uid = event.source.user_id
-        if self.message_from_user in test_terms:
-            try:
-                self.test(event)
-            except Exception as e:
-                raise Exception('Error xxxx')
-
-        if self.message_from_user in linking_terms:
-            try:
-                self.send_account_linking_url(event)
-            except Exception as e:
-                raise Exception('Error xxxx')
+        try:
+            msg_handler()
+        except Exception as e:
+            # TODO: create wrapper and logging
+            raise Exception('An error occurred')
 
     def test(self, event) -> None:
         obj = UserDetail.objects.all().filter(line_message_uid=self.line_message_uid).first()
@@ -100,9 +97,9 @@ class LineBot(LineBotBase):
             like_ingredients = []
         else:
             like_ingredients: list = like_ingredients_raw[1:-1].split(',')
-        suggest_link = _tmp_food_search_func(like_ingredients)
+        suggest_link = search_recipe.tmp_food_search_func(like_ingredients)
         msg = TextSendMessage(text=suggest_link)
-        line_bot_api.push_message(self.line_message_uid, messages=self.message_from_user)
+        line_bot_api.push_message(self.line_message_uid, messages=msg)
         account_link_url = f'{self.line_message_uid}\n This is test{suggest_link}'
         msg = TextSendMessage(text=account_link_url)
         line_bot_api.reply_message(event.reply_token, msg)
